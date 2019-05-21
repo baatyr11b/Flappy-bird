467f2f1a87a71e27fa78ebc6d90cabbf32f06c45
baatyr11b <baatyr.birnazarov@iaau.edu.kg>
2019-05-22 02:01:56 +0600

    commit all edited files

---
+++ src/game/essentials/Music.java
@@ -0,0 +1,29 @@
+package game.essentials;
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.io.File;
+
+public class Music extends Thread
+{
+    private String filename;
+    public Music(String file) {
+        filename = file;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        try {
+            String musicFile = filename;
+            Media sound = new Media(new File(musicFile).toURI().toString());
+            MediaPlayer mediaPlayer = new MediaPlayer(sound);
+            mediaPlayer.setVolume(0.3);
+            mediaPlayer.setCycleCount(1);
+            mediaPlayer.play();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
